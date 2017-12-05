cdefb45017682ae62d6907f2899fd6dbac48cd3e
chrisworry <wangrui22bd@gmail.com>
2017-12-05 23:28:00 +0800

    progress

--- data_interaction/www/js/upload.js
+++ data_interaction/www/js/upload.js
@@ -1,13 +1,14 @@
-function upload(oFile) {
+function upload(oFile, uploadDone, progress) {
     Array.from(oFile.files).forEach(file => {
         let fileReader = new FileReader();
-        let interface = '/upload';
-        if(-1 != file.type.search('image')) {
-            fileReader.readAsDataURL(file);
-            interface = '/upload_base64';
-        } else {
-            fileReader.readAsText(file);
-        }
+        let interface = '/upload_base64';
+        fileReader.readAsDataURL(file);
+        // if(-1 != file.type.search('image')) {
+        //     fileReader.readAsDataURL(file);
+        //     interface = '/upload_base64';
+        // } else {
+        //     fileReader.readAsText(file);
+        // }
         
         fileReader.onload = function () {
             console.log('read success');
@@ -18,31 +19,49 @@ function upload(oFile) {
             xhr.upload.onload = function () {
                 console.log('upload done.')
             }
-            // xhr.upload.onprogress = function (ev) {
-            //     console.log(ev.total + ' ' + ev.loaded);
-            // }
-            // xhr.upload.onloadend = function (err) {
-            //     console.log(`upload end:${err}`);
-            // }
+            
+            xhr.upload.onprogress = function (ev) {
+                //console.log(ev.total + ' ' + ev.loaded);
+                if (progress) {
+                    progress(Math.floor(100*ev.loaded/ev.total));
+                }
+            }
+            xhr.upload.onloadend = function () {
+
+            }
 
             let formData = new FormData();
             //readerFile.result 存储了读取的结果
             //console.log(this.result);
             if (interface == '/upload_base64') {
                 formData.append('file', this.result);
-            } else {
+            } else if(interface == '/upload'){
                 formData.append('file', this.result);
             }
             
             xhr.open('POST', interface, true);
             xhr.send(formData);
 
+
+            //readerFile.result 存储了读取的结果
+            //console.log(this.result);
+            // xhr.open('POST', interface, true);
+            // if (interface == '/upload_base64') {
+            //     xhr.setRequestHeader('Content-type', 'multipart/form-data');
+            //     xhr.send(fileReader.result);
+            // } else if(interface == '/upload'){
+            //     xhr.setRequestHeader('Content-Type','multipart/form-data');
+            //     xhr.send(fileReader.result);
+            // }
+            
             xhr.onreadystatechange = function (data) {
                 if (xhr.readyState == 4) {
                     if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 404) {
-                        console.log('upload success.');
+                        if(uploadDone) {
+                            uploadDone(null, xhr.responseText);
+                        }
                     } else {
-                        console.log("upload failed.");
+                        uploadDone("upload failed.", "");
                     }
                 }
             }
