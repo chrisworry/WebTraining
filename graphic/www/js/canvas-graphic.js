bf65b928448cb8dbaa2715b380e4e2184ded50c6
chrisworry <wangrui22bd@gmail.com>
2017-12-12 21:27:28 +0800

    add hit move

--- graphic/www/js/canvas-graphic.js
+++ graphic/www/js/canvas-graphic.js
@@ -6,6 +6,10 @@ class Shape {
         this._fillColor = 'rgba(255,0,0,0.5)';
         this._strokeColor = 'black';
         this._strokeWidth = 2;
+        this._highLight = false;
+        this._highLightColor = 'rgba(255,0,0,1)';
+        this._disX =0;
+        this._disY =0;
     }
 
     fillColor(color) {
@@ -22,6 +26,15 @@ class Shape {
 
     draw() {
 
+    }
+    hit(x, y) {
+        return false;
+    }
+    move(x,y) {
+
+    }
+    highLight(flag) {
+        this._highLight = flag;
     }
 };
 
@@ -62,7 +75,7 @@ class Path extends Shape {
                 this.ctx.lineTo(this.data[i], this.data[i + 1]);
             }
             this.ctx.closePath();
-            this.ctx.fillStyle = this._fillColor; 
+            this.ctx.fillStyle = this._fillColor;
             this.ctx.fill();
         }
 
@@ -79,33 +92,31 @@ class Path extends Shape {
         }
         this.ctx.stroke();
 
-        
-        
     }
 };
 
 class Rect extends Shape {
     constructor(canvas) {
-        super(canvas,'rect')
+        super(canvas, 'rect')
         this._x = 0;
         this._y = 0;
         this._w = 0;
         this._h = 0;
     }
 
-    setRect(x,y,width,height) {
+    setRect(x, y, width, height) {
         this._x = x;
         this._y = y;
         this._w = width;
         this._h = height;
     }
 
-    setXY(x,y) {
+    setXY(x, y) {
         this._x = x;
         this._y = y;
     }
 
-    setSize(width,height) {
+    setSize(width, height) {
         this._w = width;
         this._h = height;
     }
@@ -115,43 +126,60 @@ class Rect extends Shape {
     }
 
     draw() {
-    
+
         //fill
         this.ctx.beginPath();
-        const x=this._x;
-        const y=this._y;
-        const w=this._w;
-        const h=this._h;
-        this.ctx.moveTo(x,y);
-        this.ctx.lineTo(x+w,y);
-        this.ctx.lineTo(x+w,y+h);
-        this.ctx.lineTo(x,y+h);
+        const x = this._x;
+        const y = this._y;
+        const w = this._w;
+        const h = this._h;
+        this.ctx.moveTo(x, y);
+        this.ctx.lineTo(x + w, y);
+        this.ctx.lineTo(x + w, y + h);
+        this.ctx.lineTo(x, y + h);
         this.ctx.closePath();
-        this.ctx.fillStyle = this._fillColor; 
+        this.ctx.fillStyle = this._fillColor;
         this.ctx.fill();
 
         //stroke
-        this.ctx.strokeStyle = this._strokeColor;
+        this.ctx.strokeStyle = this._highLight ? this._highLightColor : this._strokeColor;
         this.ctx.lineWidth = this._strokeWidth;
         this.ctx.beginPath();
-        this.ctx.moveTo(x,y);
-        this.ctx.lineTo(x+w,y);
-        this.ctx.lineTo(x+w,y+h);
-        this.ctx.lineTo(x,y+h);
+        this.ctx.moveTo(x, y);
+        this.ctx.lineTo(x + w, y);
+        this.ctx.lineTo(x + w, y + h);
+        this.ctx.lineTo(x, y + h);
         this.ctx.closePath();
         this.ctx.stroke();
+    }
+
+    hit(x, y) {
+        if (x >= this._x && x <= this._x + this._w &&
+            y >= this._y && y <= this._y + this._h) {
+            this._disX = x-this._x;
+            this._disY = y-this._y;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    move(x,y) {
+        if (this._highLight) {
+            this._x = x - this._disX;
+            this._y = y - this._disY; 
+        }
     }
 };
 
 class Circle extends Shape {
     constructor(canvas) {
-        super(canvas,'circle');
+        super(canvas, 'circle');
         this._cx = 0;
         this._cy = 0;
         this._r = 0;
     }
 
-    setCenter(x,y) {
+    setCenter(x, y) {
         this._cx = x;
         this._cy = y;
     }
@@ -164,25 +192,43 @@ class Circle extends Shape {
         if (this._fillColor != 'rgba(0,0,0,0)') {
             this.ctx.fillStyle = this._fillColor;
             this.ctx.beginPath();
-            this.ctx.arc(this._cx,this._cy,this._r,0,2.0*Math.PI,true);
+            this.ctx.arc(this._cx, this._cy, this._r, 0, 2.0 * Math.PI, true);
             this.ctx.closePath();
             this.ctx.fill();
-    
+
         }
 
-        this.ctx.strokeStyle = this._strokeColor;
+        this.ctx.strokeStyle = this._highLight ? this._highLightColor : this._strokeColor;
         this.ctx.lineWidth = this._strokeWidth;
         this.ctx.beginPath();
-        this.ctx.arc(this._cx,this._cy,this._r,0,2.0*Math.PI,true);
+        this.ctx.arc(this._cx, this._cy, this._r, 0, 2.0 * Math.PI, true);
         this.ctx.closePath();
         this.ctx.stroke();
+    }
+
+    hit(x, y) {
+        const dis = Math.sqrt((x-this._cx)*(x-this._cx) + (y-this._cy)*(y-this._cy));
+        if (dis <= this._r) {
+            this._disX = x-this._cx;
+            this._disY = y-this._cy;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    move(x,y) {
+        if (this._highLight) {
+            this._cx = x - this._disX;
+            this._cy = y - this._disY; 
+        }
     }
 };
 
 class ShapeContainer {
     constructor(canvas) {
         this.canvas = canvas;
-        this.shapes = [];    
+        this.shapes = [];
     }
 
     push(shape) {
@@ -198,14 +244,14 @@ class ShapeContainer {
     }
 
     rear() {
-        if (this.shapes.length == 0){
+        if (this.shapes.length == 0) {
             return null;
         } else {
-            return this.shapes[this.shapes.length-1];
+            return this.shapes[this.shapes.length - 1];
         }
     }
 
-    render(clearCanvas=true) {
+    render(clearCanvas = true) {
         if (clearCanvas) {
             this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width, this.canvas.height);
         }
@@ -213,5 +259,28 @@ class ShapeContainer {
             ele.draw();
         });
     }
-    
+
+    hit(x,y) {
+        this.clearHit();
+        for (let i=this.shapes.length-1; i>=0; --i) {
+            if (this.shapes[i].hit(x,y)) {
+                this.shapes[i].highLight(true);
+                return this.shapes[i];
+            }
+        }
+        return null;
+    }
+
+    clearHit() {
+        this.shapes.forEach(ele=>{
+            ele.highLight(false);
+        });
+    }
+
+    move(x,y) {
+        this.shapes.forEach(ele=>{
+            ele.move(x,y);
+        });
+    }
+
 };
