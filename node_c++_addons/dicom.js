fab8ffb651b45a7d0a727ef47efdad29f2527047
wangrui22 <wangrui22@baidu.com>
2018-01-22 11:20:15 +0800

    add cide

--- node_c++_addons/dicom.js
+++ node_c++_addons/dicom.js
@@ -1,4 +1,8 @@
 
+let myJson = {};
+myJson.push({'123':12});
+console.log(myJson);
+
 function getDICOMModule() {
     try {
         return require('./build/Release/dicom.node');
@@ -10,12 +14,14 @@ function getDICOMModule() {
 const dicom = getDICOMModule();
 const dicomPath = '/home/wangrui22/data/AB_CTA_01/DICOM7_000000.dcm';
 
-dicom.loadAsBuffer(dicomPath, function(err, dcm_info, buffer) {
-    if (err) {
-        console.log('load As buffer failed.');
-    } else {
-        console.log('load As buffer success.');
-        let header = JSON.parse(dcm_info);
-        console.log(header);
-    }
-});
+
+
+// dicom.loadAsBuffer(dicomPath, function(err, dcm_info, buffer) {
+//     if (err) {
+//         console.log('load As buffer failed.');
+//     } else {
+//         console.log('load As buffer success.');
+//         let header = JSON.parse(dcm_info);
+//         console.log(header);
+//     }
+// });
